0e652f09b4065a971987aee755c61c5b5d557d00
Ygor Fonseca <ygor.luiz.fonseca@gmail.com>
2020-10-07 15:38:25 -0300

    Removing Uneeded Files & Adding Structure

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+// Libs
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        Home
+      </div>
+    )
+  }
+}
+
+export default Home;
