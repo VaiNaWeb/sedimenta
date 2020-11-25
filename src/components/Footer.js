3e98e931e3938ce5661bf4d142196205015ce59d
Ygor Fonseca <ygor.luiz.fonseca@gmail.com>
2020-11-25 02:29:40 -0300

    Removing Links Demonstration Section For Now

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,71 +4,6 @@ import styled from 'styled-components';
 import Image from '../assets/image.svg';
 import LogoVnW from '../assets/logoVnw.png';
 
-const LinkLinha = styled.div`
-  width: 93%;
-  height: 1vh;
-  background: linear-gradient(to right, #992836, #4D141B);
-  margin: 1rem 0 0.3rem;
-
-  @media (max-width: 648px) {
-    display: none;
-	}
-`;
-
-const ContentLink = styled.div`
-  width: 93%;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-
-  @media (max-width: 648px) {
-    width: 90%;
-    border-left: 7px solid #992836;
-    margin-bottom: 7rem;
-	}
-`;
-
-const ContentLinkGeral = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-
-  @media (max-width: 648px) {
-    flex-direction: column;
-    flex-wrap: wrap;
-    margin-left: 1rem;
-	}
-`;
-
-const SeparationLink = styled.div`
-  width: 25%;
-
-  @media (max-width: 648px) {
-    width: 85%;
-    display: flex;
-    justify-content: space-between;
-	}
-`;
-
-const ParagraphLink = styled.p`
-  color: #373737;
-  font-size: 0.9rem;
-  font-family: 'Open Sans', ExtraBold;
-  font-weight: bold;
-  margin-bottom: 3rem;
-
-  @media (max-width: 648px) {
-    font-size: 1rem;
-    margin-left: 1rem;
-	}
-`;
-const Link = styled.a`
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 5rem;
-  text-decoration: none;
-`;
-
 const ContentFooter = styled.div`
   width: 90%;
   height: 45vh;
@@ -222,24 +157,6 @@ class Footer extends Component {
   render() {
     return (
       <>
-      <LinkLinha></LinkLinha>
-        <ContentLink>
-          <ParagraphLink>LINKS IMPORTANTES</ParagraphLink>
-          <ContentLinkGeral>
-            <SeparationLink>
-              <Link href="https://github.com/Erlane" target="_blank">Link 1</Link>
-              <Link href="https://github.com/Erlane" target="_blank">Link 2</Link>
-            </SeparationLink>
-            <SeparationLink>
-              <Link href="https://github.com/Erlane" target="_blank">Link 3</Link>
-              <Link href="https://github.com/Erlane" target="_blank">Link 4</Link>
-            </SeparationLink>
-            <SeparationLink>
-              <Link href="https://github.com/Erlane" target="_blank">Link 5</Link>
-              <Link href="https://github.com/Erlane" target="_blank">Link 6</Link>
-            </SeparationLink>
-          </ContentLinkGeral>
-        </ContentLink>
         <ContentFooter>
           <ContentFooterRede>
             <ContentFooterContact>
@@ -274,7 +191,13 @@ class Footer extends Component {
         </ContentFooter>
         <FooterFinish>
           <p>DESENVOLVIDO POR:</p>
-          <img src={LogoVnW} alt='logoVnw' />
+          <a
+            href='https://www.vainaweb.com.br/'
+            target='_blank'
+            rel="noopener noreferrer"
+          >
+            <img src={LogoVnW} alt='logoVnw' />
+          </a>
         </FooterFinish>
       </>
     );
